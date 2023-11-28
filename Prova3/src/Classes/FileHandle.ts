976356ccbb746b715b72d48a10321b53a4e89795
Eduardo270704 <luis270704@gmail.com>
2023-11-28 20:11:08 -0300

    P3

--- Prova3/src/Classes/FileHandle.ts
+++ Prova3/src/Classes/FileHandle.ts
@@ -7,47 +7,41 @@ export class FileHandler {
   constructor(filename: string = "dados.txt") {
     this.filename = path.resolve(__dirname, filename);
   }
-  private async fileExists(): Promise<boolean> {
-    try {
-      await fs.access(this.filename, fs.constants.F_OK);
-      return true;
-    } catch (error) {
-      return false;
-    }
-  }
   async read(): Promise<LogProps[]> {
     try {
-      if (await this.fileExists()) {
-        const data = await fs.readFile(this.filename, "utf-8");
-        const lines = data.split("\n").filter(Boolean); // Filtra linhas vazias
-        const logs: LogProps[] = lines.map((line) => JSON.parse(line));
-        return logs;
-      } else {
+      const data = await fs.readFile(this.filename, "utf-8");
+      const lines = data.split("\n").filter(Boolean); // Filtra linhas vazias
+      const logs: LogProps[] = lines.map((line) => JSON.parse(line));
+      return logs;
+    } catch (error: any) {
+      if (error.code === "ENOENT") {
+        // Arquivo não encontrado, retorna um array vazio
         return [];
+      } else {
+        // Outro erro, imprime o erro e lança uma exceção
+        console.error("Error reading file:", error);
+        throw error;
       }
-    } catch (error) {
-      console.error("Error reading file:", error);
-      throw error;
     }
   }
   async write(logs: LogProps[]): Promise<void> {
     try {
-      await fs.access(this.filename);
       const lines = logs.map((log) => JSON.stringify(log));
-      const data = lines.join("\n") + "\n";
-      await fs.writeFile(this.filename, data, "utf-8");
-    } catch (error) {
+      const data = lines.join("\n") + "\n"; // Adiciona uma nova linha ao final
+
+      // Usamos { flag: 'w' } para abrir o arquivo em modo de escrita
+      await fs.writeFile(this.filename, data, { flag: "w", encoding: "utf-8" });
+    } catch (error: any) {
       console.error("Error writing file:", error);
       throw error;
     }
   }
   async append(log: LogProps): Promise<void> {
     try {
-      await this.read();
       const logs = await this.read();
       logs.push(log);
       await this.write(logs);
-    } catch (error) {
+    } catch (error: any) {
       console.error("Error appending to file:", error);
       throw error;
     }
